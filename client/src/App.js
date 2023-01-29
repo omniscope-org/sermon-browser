dc82981c47ef0541dea32061ba492ce89ae04ab8
Artem Ermakov <nawarius1993@gmail.com>
2023-01-29 19:10:03 +0400

    Adds: Title. Refactor: Clear code (Video)

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,17 @@ import uploadXlsx from './helpers/xlsx/uploadXlsx'
 
 function App () {
   const [data, setData] = useState([])
-  
+
   const [selected, setSelected] = useState({})
   const handleSelect = (item) => setSelected(item)
   
-  return <div style = {{width: '100vw',  height: '100vh'}}>
+  return <div style = {{width: '100vw', height: '100vh'}}>
 
     <input type="file" id="uploadXlsx" style = {{display: 'none'}} onChange = {(e) => uploadXlsx(e, setData)} />
     <button onClick = {() => {document.getElementById('uploadXlsx').click()}}>Upload XLSX</button>
 
-    <div style = {{width: '100%', height: 'auto', minHeight: '50%',
-      display: 'flex', justifyContent: 'space-around', border: '2px solid red', marginTop: 10
+    <div style = {{width: '100%', minHeight: window.innerHeight / 2,
+      display: 'flex', justifyContent: 'space-around', marginTop: 10
     }}>
       <Map data = {data} handleSelect = {handleSelect} />
       <Video videoId = {selected.youtubeId} timecode = {selected.timecode} />
--- client/src/Components/Video.jsx
+++ client/src/Components/Video.jsx
@@ -4,32 +4,15 @@ import YouTube from 'react-youtube'
 const Video = ({videoId, timecode}) => {
 
     const opts = {
-        //height: '390',
-        //width: '640',
-        playerVars: {
-          autoplay: 1,
-          start: timecode
-        },
-      };
+      height: window.innerHeight / 2,
+      playerVars: {
+        autoplay: 1,
+        start: timecode
+      },
+    }
 
     return <div style = {{width: '45%', height: '100%', position: 'relative'}}>
-        {videoId && 
-        <YouTube videoId = {videoId} opts={opts} style = {{height: '100%'}} />
-            // <video 
-            //     controls 
-            //     className ="video-js vjs-big-play-centered vjs-fluid"
-            //     data-setup = {
-            //     `{ 
-            //         "techOrder": ["youtube"], 
-            //         "sources": [{ 
-            //             "type": "video/youtube", 
-            //             "src": "https://www.youtube.com/watch?v=${videoId}"
-            //         }],
-            //         "youtube": { "start": ${timecode} } 
-            //     }`
-            //     }
-            // ></video>
-        }
+        {videoId && <YouTube videoId = {videoId} opts={opts} />}
     </div>
 }
 
